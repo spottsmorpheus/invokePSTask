5f9305f1bad20359f59d4190365af032625b9c03
Morpheus Steve Potts <morpheus@Morpheuss-MacBook-Pro.local>
2022-07-25 18:52:20 +0100

    Example cmdExample.ps1

---
+++ Examples/cmdExample.ps1
@@ -0,0 +1,6 @@
+Write-Host "This is and Example Powershell Cmd Script executing from a Git Url"
+Write-Host ""
+Write-Host "Executing on $([Environment]::MachineName) - User $([Environment]::UserName)"
+Write-Host "Command Line: $([Environment]::CommandLine)"
+# Signal a good exit
+Exit 0
